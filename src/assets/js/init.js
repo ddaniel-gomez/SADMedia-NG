99ac8bf39b72c76850b0358a1f6f3dec23f35f5d
DanielSan <ddaniel.gomez@gmail.com>
2018-09-04 20:04:23 +0200

    contact page

--- src/assets/js/init.js
+++ src/assets/js/init.js
@@ -6,5 +6,7 @@
     $("html, body").animate({ scrollTop: 0}, 600);
     scrollme.init();
     Materialize.updateTextFields();
+    //location reload on contact
+    console.log("location: " + window.location.pathname);
   }); // end module
 })(jQuery); // end of jQuery name space
